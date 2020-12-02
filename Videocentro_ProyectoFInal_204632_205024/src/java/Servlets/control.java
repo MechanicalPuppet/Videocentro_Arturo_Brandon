b55d8254d7f049a485186c836523adebb7e6d77f
Brandon Hernandez <56750116+MechanicalPuppet@users.noreply.github.com>
2020-12-02 00:45:40 -0600

    Ya tenemos el index para empezar a hacer el control y los servlets

--- Videocentro_ProyectoFInal_204632_205024/src/java/Servlets/control.java
+++ Videocentro_ProyectoFInal_204632_205024/src/java/Servlets/control.java
@@ -31,19 +31,34 @@ public class control extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet control</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet control at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+         String tareaSeleccionada= request.getParameter("tarea");
+         
+         if(tareaSeleccionada.equals("agregarVideojuego")){
+            response.sendRedirect("capturaVideojuego.html");
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            response.sendRedirect("editaVideojuego.html");
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
+        } else if (tareaSeleccionada.equals("editarVideojuego")){
+            
         }
+         
+         
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
